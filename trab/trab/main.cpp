8d26c4fc9488bd4d728dddc594a1a5cd2d392e65
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2015-11-11 21:58:08 -0200

    first commit

---
+++ trab/trab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  trab
+//
+//  Created by Hariel Giacomuzzi on 11/11/15.
+//  Copyright © 2015 Hariel Giacomuzzi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
